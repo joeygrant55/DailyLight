115388ed72ba36f73eb8ba263b23be23da0b3eed
joeygrant55 <48562179+joeygrant55@users.noreply.github.com>
2025-08-29 20:24:55 -0400

    Initial Commit

---
+++ DailyLight/DailyLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyLightApp.swift
+//  DailyLight
+//
+//  Created by Joseph Grant on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
